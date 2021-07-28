82009ad57302588a1570e1a02ab3a32ff10c4081
Muhhamed Sallam <m.sallam2622000@gmail.com>
2021-07-29 01:25:57 +0200

    first commit

---
+++ lib/input_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(color: Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(color: Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(color: Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(color: Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(color: Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+  ReusableCard({@required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0), color: color),
+    );
+  }
+}
